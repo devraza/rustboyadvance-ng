c7b445bac44412e697d523d5fd56360f229263c1
Muhammad Nauman Raza <devraza@skiff.com>
2024-03-25 22:53:53 +0000

    flake: a mixture of hotfixes and updates

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Rust development environment for oxitoko using fenix";
+  description = "Rust development environment for rustboyadvance-ng";
 
   inputs = {
     utils.url = "github:numtide/flake-utils";
@@ -16,29 +16,25 @@
     (
       system: let
         pkgs = import nixpkgs-unstable { inherit system; };
-        toolchain = pkgs.fenix.complete;
         dependencies = with pkgs; [ SDL2 SDL2_image ];
       in rec
       {
         # Executed by `nix build`
-        packages.default =
-          (pkgs.makeRustPlatform.buildRustPackage {
-            pname = "rustboyadvance-ng";
-            name = "rustboyadvance-ng";
-            src = ./.;
-            meta.mainProgram = "rustboyadvance-sdl2";
-            cargoLock = {
-              lockFile = ./Cargo.lock;
-              outputHashes = {
-                "libretro-backend-0.2.1" = "sha256-qsJo7wP01zhRNv4XrZJbIvOQrSJfUaqve0fNOaR6aWs=";
-              };
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "rustboyadvance-ng";
+          name = "rustboyadvance-ng";
+          src = ./.;
+          meta.mainProgram = "rustboyadvance-sdl2";
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+            outputHashes = {
+              "libretro-backend-0.2.1" = "sha256-qsJo7wP01zhRNv4XrZJbIvOQrSJfUaqve0fNOaR6aWs=";
             };
-            buildInputs = dependencies;
           };
-
+          buildInputs = dependencies;
+        };
         # Executed by `nix run`
         apps.default = utils.lib.mkApp {drv = packages.default;};
-
         # Used by `nix develop`
         devShells.default = pkgs.mkShell rec { buildInputs = dependencies; };
       }
